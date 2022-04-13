a2c7908f05c6aa55e03185e3e0c552cef636503d
Johnathan Fludd <45986771+JohnathanFludd@users.noreply.github.com>
2022-04-12 22:43:05 -0500

    Add files via upload

---
+++ Leaflet-Step-1/static/js/config.js
@@ -0,0 +1,8 @@
+// API key
+const API_KEY = "YOUR API KEY HERE!";
+
+
+
+// Store our API endpoint as queryUrl.
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
